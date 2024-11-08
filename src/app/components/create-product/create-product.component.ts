e8b237530a1a068355b70711feb1b2e870eb94f3
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-11-08 15:13:43 +0200

    feat: add modal component

---
+++ src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html'
+})
+export class CreateProductComponent implements OnInit {
+
+  form = new FormGroup({
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  })
+
+  get title() {
+    return this.form.controls.title as FormControl
+  }
+  ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.form.value)
+  }
+
+}
