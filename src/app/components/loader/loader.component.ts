9dff72cb983d513bf1fdd7eb03b8634f16d72c33
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-11-11 22:41:51 +0200

    refactor: optimize component loader

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -3,10 +3,17 @@ import { ErrorService } from '../../services/error.service';
 
 @Component({
   selector: 'app-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  templateUrl: './loader.component.html'
 })
 export class LoaderComponent implements OnInit {
+
+  loaderItems = [
+    { left: '0px', delay: '0ms' },
+    { left: '12px', delay: '200ms' },
+    { left: '24px', delay: '400ms' },
+    { left: '36px', delay: '600ms' },
+    { left: '48px', delay: '800ms' },
+  ];
   constructor(public errorService: ErrorService) {
   }
   ngOnInit(): void {
